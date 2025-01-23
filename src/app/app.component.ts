ebfb8347e9025861f11b94c8e3becb4ce98149f5
ferzamo <fernandozd19@gmail.com>
2025-01-24 00:46:14 +0100

    git hooks with husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-19-template';
+  title: string = 'angular-19-template';
 }
