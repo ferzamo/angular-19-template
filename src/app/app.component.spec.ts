b9e7539ca3324399cb154db1b260481a4eb49ddf
ferzamo <fernandozd19@gmail.com>
2025-01-24 00:59:10 +0100

    prueba error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'angular-19-template' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-19-template');
+    expect(app.title).toEqual('angular-19-templatea');
   });
 
   it('should render title', () => {
