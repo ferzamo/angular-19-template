528e133753d0e02f8ce439bcaf11232c80ad6e5a
ferzamo <fernandozd19@gmail.com>
2025-01-28 21:05:00 +0100

    feat: store computed tests

--- src/app/core/store/tests/app.store.spec.ts
+++ src/app/core/store/tests/app.store.spec.ts
@@ -3,8 +3,16 @@ import { AppStore } from '../app.store';
 import { INITIAL_STATE } from '../constants/store.constants';
 
 describe('AppStore', () => {
-  it('should', () => {
+  it('should initialize with the initial state', () => {
     const store = TestBed.inject(AppStore);
     expect(store.title()).toHaveLength(INITIAL_STATE.title.length);
+  });
+
+  it('should compute the correct titleLength', () => {
+    const store = TestBed.inject(AppStore);
+    expect(store.titleLength()).toBe(INITIAL_STATE.title.length);
+
+    store.updateTitle('New Title');
+    expect(store.titleLength()).toBe('New Title'.length);
   });
 });
