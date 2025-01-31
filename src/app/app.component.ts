f42869d9e1c4ff239b5795eec59d7737c92c60da
ferzamo <fernandozd19@gmail.com>
2025-01-31 19:24:03 +0100

    feat: ngx-translate added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppStore } from './core/store/app.store';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   providers: [AppStore],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { DEFAULT_LANGUAGE } from './configs/language.config';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: DEFAULT_LANGUAGE,
+    }),
+  ],
 };
